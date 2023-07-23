64e3e0f48c2450245abb5e20e0c663a52be89372
Daniel Mestres <daniel.mestres@upr.edu>
2023-07-23 10:57:44 -0400

    small update

--- eww/launch.sh
+++ eww/launch.sh
@@ -6,5 +6,5 @@ fi
 # Create a new instance
 if ! pgrep -x "eww" > /dev/null; then
   $HOME/apps/eww/target/release/eww daemon
-  $HOME/apps/eww/target/release/eww open bar
+  $HOME/apps/eww/target/release/eww open bar_left
 fi
